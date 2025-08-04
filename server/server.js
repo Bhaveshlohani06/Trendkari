023399d2e923d0f258a9eb558ccb106f44b17861
Bhavesh <Bhaveshlohani06@gmail.com>
2025-08-04 12:27:18 +0530

    callback url

--- server/server.js
+++ server/server.js
@@ -26,20 +26,20 @@ const allowedOrigins = [
 
 
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS: ' + origin));
-//     }
-//   },
-//   credentials: true,
-// }));
-
 app.use(cors({
-  origin: '*',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS: ' + origin));
+    }
+  },
+  credentials: true,
 }));
+
+// app.use(cors({
+//   origin: '*',
+// }));
 
 
 
