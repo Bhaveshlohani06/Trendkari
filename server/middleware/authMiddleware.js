0bc16e4aa46cafde8f97b69b933e6fdd59fe80d6
Bhavesh <Bhaveshlohani06@gmail.com>
2025-08-07 10:02:46 +0530

    bug

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -58,7 +58,7 @@ export const requireSignIn = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log("Decoded JWT:", decoded);
 
-    const user = await userModel.findById(decoded._id).select("-password");
+    const user = await userModel.findById(decoded.id).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
 
     req.user = user;
